f125321e7ff2fbb351c7f386ccbc28f807908b50
TPFinalGroup <33198643+TPFinalGroup@users.noreply.github.com>
2017-10-29 17:23:17 +0200

    add tree view

---
+++ src/main/java/TheatreTicketBookingSystem/repositories/AdminRepository.java
@@ -0,0 +1,7 @@
+package TheatreTicketBookingSystem.repositories;
+
+/**
+ * Created by Hasan on 10/29/2017.
+ */
+public interface AdminRepository {
+}
---
+++ src/main/java/TheatreTicketBookingSystem/repositories/Impl/AdminRepositoryImpl.java
@@ -0,0 +1,7 @@
+package TheatreTicketBookingSystem.repositories.Impl;
+
+/**
+ * Created by Hasan on 10/29/2017.
+ */
+public class AdminRepositoryImpl {
+}
